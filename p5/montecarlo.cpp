dc2c571bf1ae79c80364b0b62cca3c36b12119b8
i12gafej <i12gafej@uco.es>
2023-12-05 13:13:44 +0100

    practica 5 termianda

--- p5/montecarlo.cpp
+++ p5/montecarlo.cpp
@@ -1,22 +1,21 @@
+#include <iostream>
 #include "montecarlo.hpp"
 #include "ClaseTiempo.hpp"
+#include <unistd.h>
+#include <vector>
 #include <random>
-#include <iostream>
 #include <cstdlib>
-#include <ctime>
 using namespace std;
 
+void imprimir(vector<int> v);   
+
 void Montecarlo(){
-    cout << "Introduce el orden de las matrices: ";
+    system("clear");
+    cout << "Introduce el orden de la matriz: ";
     int n;
     cin >> n;
-
-    vector<vector<int>> A, B, C;
-
+    vector<vector<int>> A, B, C, AB = vector<vector<int>>(n, vector<int>(n));
     rellenarMatrices(n, A, B);
-
-    vector<vector<int>> AB=vector<vector<int>>(n, vector<int>(n));
-
     Clock time;
 
     time.start();
@@ -24,121 +23,34 @@ void Montecarlo(){
     time.stop();
     long long t1 = time.elapsed();
 
-    if(lanzarDado()){
+    if(lanzarDado() % 2 == 0){
         C = AB;
     }
     else{
         AB[n/2][n/2] += 1;
         C = AB;
     }
-
     cout << "Introduzca el numero de pruebas a realizar: ";
     int nPruebas;
     cin >> nPruebas;
-    
-    time.start();
-    verificarProductoMontecarlo(A,B,C, nPruebas);
-    cout <<"FUERAA"<<endl;
-    time.stop();
-    long long t2 = time.elapsed();
-    
-    
-    // if(intentos_exitosos != nPruebas){
-    //     cout << "Numero de pruebas que se han necesitado: "<<intentos_exitosos <<endl;
-    // }else{
-    //     float p = 1.0 - (1.0 / static_cast<float>(2, nPruebas));
-    //     cout << "La probabilidad de verificación es: "<< p << endl;
-    // }
+    Clock time2;
+    time2.start();
+    int exitosas = repetirVerificarProductoMontecarlo(A, B, C, n, nPruebas);
+    time2.stop();
+    long long t2 = time2.elapsed();
+    if(exitosas != nPruebas){
+        cout << "Numero de pruebas que se han necesitado: "<<exitosas <<endl;
+    }else{
+        float p = 1.0 - (1.0 / static_cast<float>(pow(2, nPruebas)));
+        cout << "La probabilidad de verificación es: "<< p << endl;
+    }
     cout << "----------------------" << endl;
     cout << "Tiempo de montecarlo: "<< t2 << endl;
-}
-int verificarProductoMontecarlo(vector<vector<int>> A,vector<vector<int>> B,vector<vector<int>> C, int n){    
-    int exitosas = 0;
-    vector<vector<int>> X, XAB, XA, XC;
-    XAB = vector<vector<int>>(A.size(), vector<int>(1));
-    XA = vector<vector<int>>(A.size(), vector<int>(1));
-    XC = vector<vector<int>>(A.size(), vector<int>(1));
+    cout << "Tiempo de producto de matrices: "<< t1 << endl;
 
-    for(int i = 0; i < n; i++){
-        cout << "Inicio"<<endl;
-        //limpiar(X);
-        if(i == 1)
-            cout << "X size:"<<X.size() << " "<<X[0].size();
-        X = generarVectorX(A.size());
-        
-        cout << "Producto XA" << endl;
-        productoMatrices(X, A, XA);
-        cout << "Producto XAB" << endl;
-        productoMatrices(XA, B, XAB);
-        cout << "Producto XC" << endl;
-        productoMatrices(X, C, XC);
-        cout << "Fin productos" << endl;
-        for(int i = 0; i < X.size(); i++){
-            for(int j = 0; j < X[0].size(); j++){
-                cout << X[i][j] << " ";
-            }
-            cout << endl;
-        }
-        cout << "-------"<<endl;
-        for(int i = 0; i < XAB.size(); i++){
-            for(int j = 0; j < XAB[0].size(); j++){
-                cout << XAB[i][j] << " ";
-            }
-            cout << endl;
-        }
-        cout << "-------"<<endl;
-        for(int i = 0; i < XC.size(); i++){
-            for(int j = 0; j < XC[0].size(); j++){
-                cout << XC[i][j] << " ";
-            }
-            cout << endl;
-        }
-        cout << "-------"<<endl;
-        if(comprobarIgualdad(XAB, XC) == true){
-            cout << "Exitosas"<<endl;
-            exitosas++;
-        }
-        else{
-            cout << "No exitosas" << endl;
-        }
-        
-    }
-    
-    return exitosas;
+
 }
-// void limpiar(vector<vector<int>> &m){
-//     for (auto& fila : m) {
-//         fila.clear(); 
-//     }
-//     m.clear(); 
-// }
-bool comprobarIgualdad( vector<vector<int>>& matrizA,  vector<vector<int>>& matrizB) {
-    for (size_t i = 0; i < matrizA.size(); ++i) {
-        cout << "i = "<<i<<endl;
-        if (matrizA[i][0] != matrizB[i][0]) {
-            cout << "Dentro" <<endl;
-            return false;
-            cout << "Otra vez"<<endl;
-        }
-    }
-    return true;
-}
-vector<vector<int>> generarVectorX(int n){
-    std::random_device rd; 
-    std::mt19937 gen(rd()); 
-    std::uniform_int_distribution<> dis(0, 1); 
-    
-    vector<vector<int>> X(1, vector<int>(n));
-    srand(static_cast<unsigned int>(time(nullptr)));
-    for(int i = 0; i < n; i++){
-        X[0][i] = dis(gen);
-    }
-    return X;
-}
-int lanzarDado(){
-    srand(static_cast<unsigned int>(time(nullptr)));
-    return (rand() % 6) + 1;
-}
+
 void rellenarMatrices(int n, vector<vector<int>>& matriz1, vector<vector<int>>& matriz2){
     matriz1=vector<vector<int>>(n, vector<int>(n));
     matriz2=vector<vector<int>>(n, vector<int>(n));
@@ -147,37 +59,91 @@ void rellenarMatrices(int n, vector<vector<int>>& matriz1, vector<vector<int>>& 
 
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            // Generar un número aleatorio entre -1, 0 y 1
             matriz1[i][j] = rand() % 3 - 1;
-            //cout << matriz1[i][j] << " ";
         }
-        //cout << endl;
     }
-    //cout << "----------------------------------" << endl;
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            // Generar un número aleatorio entre -1, 0 y 1
             matriz2[i][j] = rand() % 3 - 1;
-            //cout << matriz2[i][j] << " ";
         }
-        //cout << endl;
     }
 }
-void productoMatrices(const vector < vector < int > > &m1, const vector < vector < int > > &m2, vector < vector < int > > &producto)
-{
-	for(unsigned int i = 0; i < m1.size(); i++) //Recorre filas de la primera matriz
-	{
-        
-		for(unsigned int j = 0; j < m2[0].size(); j++) //Recorre columnas de la segunda matriz
-		{
-            
+void productoMatrices(const vector < vector < int > > &m1, const vector < vector < int > > &m2, vector < vector < int > > &producto){
+	for(unsigned int i = 0; i < m1.size(); i++){
+		for(unsigned int j = 0; j < m2[0].size(); j++){
 			producto[i][j] = 0.0;
-            
-			for(unsigned int k = 0; k < m1[0].size(); k++){ //Recorre los elementos a multiplicar (numero de columnas de la primera matriz)
-                
+			for(unsigned int k = 0; k < m1[0].size(); k++){ 
 				producto[i][j] = producto [i][j] + m1[i][k]*m2[k][j];
                 
             }
+		}
+	}
+}
+int lanzarDado(){
+    srand(static_cast<unsigned int>(time(nullptr)));
+    return (rand() % 6) + 1;
+}
+int repetirVerificarProductoMontecarlo(vector<vector<int>> A,vector<vector<int>> B,vector<vector<int>> C, int n, int nPruebas){
+    int exitosas = 0;
+    for(int i = 0; i < nPruebas; i++){
+        if(verificarProductoMontecarlo(A, B, C, A.size())){
+            exitosas++;
+        }
+    }
+    return exitosas;
+}
+bool verificarProductoMontecarlo(vector<vector<int>> A,vector<vector<int>> B,vector<vector<int>> C, int n){
+    vector<int> XAB, XA, XC;
+    vector<int> X;
+    XAB = vector<int>(n);
+    XA = vector<int>(n);
+    XC = vector<int>(n);
+    X = generarVectorX(n);
+    productoMatrices2(X, A, XA);
+    productoMatrices2(XA, B, XAB);
+    
+   
+    productoMatrices2(X, C, XC);
+    
+    
+    if(comprobarIgualdad(XAB, XC) == true){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+void imprimir(vector<int> v){
+    for(int i = 0; i < v.size(); i++){
+        cout << v[i] << ", ";
+    }
+    cout << endl;
+}
+vector<int> generarVectorX(int n){
+    std::random_device rd; 
+    std::mt19937 gen(rd()); 
+    std::uniform_int_distribution<> dis(0, 1); 
+    
+    vector<int> X(n);
+    srand(static_cast<unsigned int>(time(nullptr)));
+    for(int i = 0; i < n; i++){
+        X[i] = dis(gen);
+    }
+    return X;
+}
+
+bool comprobarIgualdad(vector<int>& matrizA, vector<int>& matrizB){
+    for (size_t p = 0; p < matrizA.size(); ++p) {
+        if (matrizA[p] != matrizB[p])
+            return false;
+    }
+    return true;
+}
+void productoMatrices2(const vector < int > &m1, const vector < vector < int > > &m2, vector < int > &producto){
+	for(unsigned int i = 0; i < m1.size(); i++){
+		producto[i] = 0.0;
+        for(unsigned int j = 0; j < m1.size(); j++){
+            producto[i] = producto [i] + m1[j]*m2[j][i];
 		}
 	}
 }
