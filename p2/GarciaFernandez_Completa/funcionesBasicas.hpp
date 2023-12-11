a3969a1c45f4c16b4e9357821ca0406039d23eb2
i12gafej <i12gafej@uco.es>
2023-12-11 20:24:16 +0100

    clean

--- p2/GarciaFernandez_Completa/funcionesBasicas.hpp
+++
@@ -1,9 +0,0 @@
-#include <vector>
-#include <iostream>
-#include <fstream>
-using namespace std;
-
-void
-rellenarVector(vector<double> &v1, vector<double> &v2);
-void
-imprimir(vector <double> qs1, vector <double> qs2, vector <int> n);
--- p2/GarciaFernandez_Fuentes/medioNivel.hpp
+++
@@ -1,5 +0,0 @@
-#include "funcionesBasicas.hpp"
-#include "quicksort.hpp"
-
-void 
-comparacionVariantesQuicksort(int nMin, int nMax, int incremento, int repeticiones, int nMediana, int nElementosMinimo);
